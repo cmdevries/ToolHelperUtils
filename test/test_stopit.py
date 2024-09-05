a2210282e0b4fe75e43e56baf0834cabeb0ace7e
Chris de Vries <chris.de.vries@quantco.com>
2024-09-05 12:24:05 +0200

    fix tests

--- test/test_stopit.py
+++ test/test_stopit.py
@@ -2,7 +2,7 @@ import pytest
 
 def test():
     import tool_helper_utils
-    with pytest.raises(ValueError) as e:
+    with pytest.raises(ImportError) as e:
         import helper
-    with pytest.raises(ValueError) as e:
+    with pytest.raises(ImportError) as e:
         import tool_helper_utils.foobar.best_tools
