03d002ea11f0258a4405491f96e064705d1d7b99
Chris de Vries <chris.de.vries@quantco.com>
2024-09-05 08:12:35 +0200

    pytest bs

--- test/test.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-def test():
-    import tool_helper_utils
-    with pytest.raises(ValueError) as e:
-        import helper
-    with pytest.raises(ValueError) as e:
-        import tool_helper_utils.foobar.best_tools
