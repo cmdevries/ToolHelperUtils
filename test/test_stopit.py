000000036ee67d84bb9ccfc38f278b5e2d2a682a
Moritz Wilksch <moritzwilksch@gmail.com>
2024-09-17 21:18:23 +0000

    minify + ci

--- test/test_stopit.py
+++ test/test_stopit.py
@@ -1,8 +1,5 @@
 import pytest
-
 def test():
-    import tool_helper_utils
-    with pytest.raises(ImportError) as e:
-        import helper
-    with pytest.raises(ImportError) as e:
-        import tool_helper_utils.foobar.best_tools
+	import tool_helper_utils
+	with pytest.raises(ImportError)as A:import helper
+	with pytest.raises(ImportError)as A:import tool_helper_utils.foobar.best_tools
